f6e7e438bfbc93d185ddeddc0a22f189e78fa9fd
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-07-22 14:48:58 +0200

    Article column count is now generated based on window width.

--- src/components/ArticleContainer.js
+++ src/components/ArticleContainer.js
@@ -1,25 +1,33 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Article from './Article';
+import {useState, useEffect} from 'react';
 
-export default class ArticleContainer extends Component {
-    render() {
-        let articleColumnCount = 3;
+const calculateArticleColumnCount = () => Math.floor((window.innerWidth - 100) / 430);
 
-        return (
-            <div className={'article-container'} style={{gridTemplateColumns: `repeat(${articleColumnCount}, max-content)`}}>
-                <div className={'article-container__column'}>
-                    <Article/>
-                </div>
-                <div className={'article-container__column'}>
-                    <Article/>
-                    <Article/>
-                </div>
-                <div className={'article-container__column'}>
-                    <Article/>
-                    <Article/>
-                    <Article/>
-                </div>
+export default function ArticleContainer() {
+    const [articleColumnCount, setArticleColumnCount] = useState(calculateArticleColumnCount());
+
+    useEffect(() => {
+        const handleResize = () => setArticleColumnCount(calculateArticleColumnCount());
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    });
+
+    return (
+        <div className={'article-container'} style={{gridTemplateColumns: `repeat(${articleColumnCount}, max-content)`}}>
+            <div className={'article-container__column'}>
+                <Article/>
             </div>
-        );
-    }
+            <div className={'article-container__column'}>
+                <Article/>
+                <Article/>
+            </div>
+            <div className={'article-container__column'}>
+                <Article/>
+                <Article/>
+                <Article/>
+            </div>
+        </div>
+    );
 }
