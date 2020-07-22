697d0e345de63c79ddfce30df40933dcc1f33a73
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-07-22 15:55:48 +0200

    Added date formatting.

--- src/models/ArticleInfo.js
+++ src/models/ArticleInfo.js
@@ -3,9 +3,12 @@ export default class ArticleInfo {
         this.title = title;
         this.image = image;
         this.text = text;
-        this.date = date;
         this.category = category;
         this.source = source;
         this.url = url;
+
+        let d = new Date(date);
+        this.date = `${(d.getDate() < 10 ? '0' : '') + d.getDate()}.${(d.getMonth() + 1 < 10 ? '0' : '') + (d.getMonth() + 1)}.${d.getFullYear()} `
+            + `${(d.getHours() < 10 ? '0' : '') + d.getHours()}:${(d.getMinutes() < 10 ? '0' : '') + d.getMinutes()}`;
     }
 }
