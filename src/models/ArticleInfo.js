c58221f756c13dc0ad6bc1ce4e086a94a6143e13
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-07-22 15:10:46 +0200

    Restructured article info management.

---
+++ src/models/ArticleInfo.js
@@ -0,0 +1,11 @@
+export default class ArticleInfo {
+    constructor(title, image, text, date, category, source, url) {
+        this.title = title;
+        this.image = image;
+        this.text = text;
+        this.date = date;
+        this.category = category;
+        this.source = source;
+        this.url = url;
+    }
+}
