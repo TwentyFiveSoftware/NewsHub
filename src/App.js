8854f9197e033788a248ed41acefcc9171db2b9d
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-07-22 16:58:59 +0200

    Separated sources and categories to be able to combine multiple sources in one category.

--- src/App.js
+++ src/App.js
@@ -8,24 +8,37 @@ import Header from './components/Header';
 import ArticleContainer from './components/ArticleContainer';
 import OptionsSection from './components/OptionsSection';
 
-const sources = [
-    new Source('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.spiegel.de%2Fschlagzeilen%2Ftops%2Findex.rss', 'SPIEGEL', true, ''),
-    new Source('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.spiegel.de%2Fschlagzeilen%2Feilmeldungen%2Findex.rss', 'SPIEGEL', true, ''),
-    new Source('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.spiegel.de%2Fschlagzeilen%2Findex.rss', 'SPIEGEL', true, ''),
+const sources = {
+    spiegelTop: new Source('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.spiegel.de%2Fschlagzeilen%2Ftops%2Findex.rss', 'Spiegel', true, ''),
+    spiegelEil: new Source('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.spiegel.de%2Fschlagzeilen%2Feilmeldungen%2Findex.rss', 'Spiegel', true, ''),
+    spiegel: new Source('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.spiegel.de%2Fschlagzeilen%2Findex.rss', 'Spiegel', true, ''),
+    sueddeutscheEil: new Source('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Frss.sueddeutsche.de%2Frss%2FEilmeldungen', 'Süddeutsche Zeitung', false, /<p>|<\/p>/g),
+}
+
+const sourceCategories = [
+    [sources.spiegelTop],
+    [sources.spiegelEil, sources.sueddeutscheEil],
+    [sources.spiegel]
 ]
 
 export default function App() {
     const [articles, setArticles] = useState([[], [], []]);
-    const [selectedArticleSourceIndex, setSelectedArticleSourceIndex] = useState(0);
+    const [selectedSourceCategoryIndex, setSelectedSourceCategoryIndex] = useState(0);
 
     useEffect(() => {
         const fetchArticles = async () => {
             let articleLists = [];
 
-            for (let i = 0; i < sources.length; i++) {
-                const result = await axios(sources[i].url);
-                articleLists[i] = result.data.items.map(article => new ArticleInfo(article.title, sources[i].providesImage ? article.enclosure.link : null,
-                    article.description.replace(sources[i].replace, ''), article.pubDate, sources[i].name, article.link));
+            for (let categoryIndex = 0; categoryIndex < sourceCategories.length; categoryIndex++) {
+                for (let source of sourceCategories[categoryIndex]) {
+                    const result = await axios(source.url);
+
+                    if (articleLists[categoryIndex] === undefined)
+                        articleLists[categoryIndex] = [];
+
+                    articleLists[categoryIndex].push(...result.data.items.map(article => new ArticleInfo(article.title, source.providesImage ? article.enclosure.link : null,
+                        article.description.replace(source.replace, ''), article.pubDate, source.name, article.link)));
+                }
             }
 
             setArticles(articleLists);
@@ -37,9 +50,9 @@ export default function App() {
     return (
         <Fragment>
             <Header/>
-            <OptionsSection onSelect={sourceIndex => setSelectedArticleSourceIndex(sourceIndex)} selected={selectedArticleSourceIndex}/>
+            <OptionsSection onSelect={index => setSelectedSourceCategoryIndex(index)} selected={selectedSourceCategoryIndex}/>
             <div className={'divider'}/>
-            <ArticleContainer articles={articles[selectedArticleSourceIndex]}/>
+            <ArticleContainer articles={articles[selectedSourceCategoryIndex]}/>
         </Fragment>
     );
 }
