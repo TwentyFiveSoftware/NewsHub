8827adaf1f5368d21a76acfb67b732c10c9cd948
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-07-22 15:30:47 +0200

    Added article fetching on start.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+import ArticleInfo from './models/ArticleInfo';
+
 import Header from './components/Header';
 import ArticleContainer from './components/ArticleContainer';
 
 export default function App() {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        const fetchArticles = async () => {
+            const result = await axios('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.spiegel.de%2Fschlagzeilen%2Ftops%2Findex.rss');
+            setArticles(result.data.items.map(article => new ArticleInfo(article.title, article.enclosure.link, article.description, article.pubDate, article.categories.join(', '), 'SPIEGEL', article.link)));
+        };
+
+        fetchArticles();
+    }, []);
+
     return (
         <div className="app">
             <Header/>
-            <ArticleContainer/>
+            <ArticleContainer articles={articles}/>
         </div>
     );
 }
--- src/components/ArticleContainer.js
+++ src/components/ArticleContainer.js
@@ -1,12 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Article from './Article';
-import {useState, useEffect} from 'react';
-
-import ArticleInfo from '../models/ArticleInfo';
 
 const calculateArticleColumnCount = () => Math.floor((window.innerWidth - 100) / 430);
 
-export default function ArticleContainer() {
+export default function ArticleContainer({articles}) {
     const [articleColumnCount, setArticleColumnCount] = useState(calculateArticleColumnCount());
 
     useEffect(() => {
@@ -16,28 +13,13 @@ export default function ArticleContainer() {
         return () => window.removeEventListener('resize', handleResize);
     });
 
-    const articleInfo = new ArticleInfo(
-        'Wirecard und Klaus-Dieter Fritsche: Kanzleramt muss weitere Kontakte einräumen',
-        'https://cdn.prod.www.spiegel.de/images/1c62c2ce-194d-49cc-a2c1-e6f88807418d_w520_r2.08_fpx50_fpy40.jpg',
-        'Wirecard hatte intensiveren Austausch mit dem Kanzleramt als bisher bekannt. Auch ein Ex-Geheimdienstbeauftragter Angela Merkels setzte sich für den mittlerweile insolventen Finanzdienstleister ein.',
-        '22.07.2020 12:18',
-        'Politik',
-        'SPIEGEL',
-        'https://www.spiegel.de/politik/deutschland/wirecard-und-klaus-dieter-fritsche-kanzleramt-muss-weitere-kontakte-einraeumen-a-4fe5ce20-e882-4b6c-adad-547dd4af10d9#ref=rss');
 
     return (
         <div className={'article-container'} style={{gridTemplateColumns: `repeat(${articleColumnCount}, max-content)`}}>
             <div className={'article-container__column'}>
-                <Article articleInfo={articleInfo}/>
-            </div>
-            <div className={'article-container__column'}>
-                <Article articleInfo={articleInfo}/>
-                <Article articleInfo={articleInfo}/>
-            </div>
-            <div className={'article-container__column'}>
-                <Article articleInfo={articleInfo}/>
-                <Article articleInfo={articleInfo}/>
-                <Article articleInfo={articleInfo}/>
+                {articles.map((article, index) =>
+                    <Article articleInfo={article} key={index}/>)
+                }
             </div>
         </div>
     );
